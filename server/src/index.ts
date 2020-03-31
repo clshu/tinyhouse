42942786abd13a65b25039219c01280490f66bef
clshu <clshu2020@gmail.com>
2020-03-31 00:38:26 -0700

    Lesson 8: Linting with ESLint. Turn off indent rules because prettier does the formating. explicit-function-return-type seems to be turned off by default already. Also VSCode setting has eslint prob which seems to be eslint.validate.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,10 @@ import express from 'express'
 const app = express()
 const PORT = process.env.NODE_ENV || 9000
 
-const one: number = 1
-const two: number = 2
+const one = 1
+const two = 2
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send(`1 + 2 = ${one + two}`)
 })
 
