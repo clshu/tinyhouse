227661f84ed0f3b4eb8610f5b3568e11f6d95bbe
clshu <clshu2020@gmail.com>
2020-03-31 05:21:19 -0700

    Module 4, Lesson 3: Connecting Server with MongoDB."

---
+++ server/src/database/index.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb'
+
+const user = 'graphql'
+const password = 'test1234'
+const cluster = 'cluster0-9ha6x'
+const uri = `mongodb+srv://${user}:${password}@${cluster}.mongodb.net/test?retryWrites=true&w=majority`
+
+export const connectDatabase = async () => {
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+
+  const db = client.db('main')
+
+  return {
+    listings: db.collection('test_listings')
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,26 @@
-import express from 'express'
+import express, { Application } from 'express'
 // import bodyParser from 'body-parser'
+import { connectDatabase } from './database'
 import { ApolloServer } from 'apollo-server-express'
 import { typeDefs, resolvers } from './graphql'
 // import { listings } from './listings'
 
-const app = express()
+// const app = express()
 const PORT = process.env.NODE_ENV || 9000
 
-const server = new ApolloServer({ typeDefs, resolvers })
-server.applyMiddleware({ app, path: '/api' })
+const mount = async (app: Application) => {
+  const db = await connectDatabase()
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  })
+  server.applyMiddleware({ app, path: '/api' })
 
-app.listen(PORT, () => console.log(`[app]: http://localhost:${PORT}`))
+  app.listen(PORT, () => console.log(`[app]: http://localhost:${PORT}`))
+
+  const listings = await db.listings.find({}).toArray()
+  console.log(listings)
+}
+
+mount(express())
