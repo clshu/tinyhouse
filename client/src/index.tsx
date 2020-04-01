e53cade4da51e16fe58d5526b09f9e08add1a4f1
clshu <clshu2020@gmail.com>
2020-04-01 01:50:12 -0700

    Module 6:Lesson 1: The Listings Component. export * from './xxx' doesn't work due to some @typescript-parser bug. import {} from './xxx' the export {} as a workaround.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Listings } from './sections'
 
 import * as serviceWorker from './serviceWorker'
 
-render(<h1>Hello World</h1>, document.getElementById('root'))
+render(<Listings />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
