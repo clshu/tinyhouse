46538918d343ed517b83c5c7ad2089612422f2f3
clshu <clshu2020@gmail.com>
2020-04-02 01:02:06 -0700

    Module 8/Lession 1: Creating our Apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 import { Listings } from './sections'
 
 import * as serviceWorker from './serviceWorker'
 
-render(<Listings title="TinyHouse Listings" />, document.getElementById('root'))
+const client = new ApolloClient({
+  uri: '/api'
+})
+
+render(
+  <ApolloProvider client={client}>
+    <Listings title="TinyHouse Listings" />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
