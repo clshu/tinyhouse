be52061ea90b259dad15ba8c41d71e557a1ab39c
clshu <clshu2020@gmail.com>
2020-04-02 18:03:09 -0700

    Part 2/3.2: Database Document Structure

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,23 +1,59 @@
 import { ObjectId, Collection } from 'mongodb'
 
+export enum ListingType {
+  Apartment = 'APARTMENT',
+  House = 'HOUSE'
+}
+
+interface BookingsIndexMonth {
+  [key: string]: boolean
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear
+}
+
 export interface Listing {
   _id: ObjectId
   title: string
+  description: string
   image: string
+  host: string // User's _id field
+  type: ListingType
   address: string
+  country: string
+  admin: string // state or province
+  city: string
+  bookingsIndex: BookingsIndex
   price: number
+  bookings: ObjectId[]
   numOfGuests: number
-  numOfBeds: number
-  numOfBaths: number
-  rating: number
+  // numOfBeds: number
+  // numOfBaths: number
+  // rating: number
 }
 
 export interface User {
-  _id: ObjectId
+  _id: string // From OAuth, a string
+  token: string
+  avatar: string
+  contact: string
+  walletId?: string
+  income: number
+  bookings: ObjectId[]
+  listings: ObjectId[]
 }
 
 export interface Booking {
   _id: ObjectId
+  listing: ObjectId
+  tenant: string // User's _id, which is string
+  checkIn: string
+  checkOut: string
 }
 
 export interface Database {
