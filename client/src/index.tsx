8a7056e9d97ddaab9c426e6441978a8b4222fd4d
clshu <clshu2020@gmail.com>
2020-04-02 17:04:12 -0700

    Part 2/2.2:React Router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,23 +2,32 @@ import React from 'react'
 import { render } from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
-import { Listings } from './sections'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Home, Host, Listing, Listings, NotFound, User } from './sections'
 import './styles/index.css'
-
-import * as serviceWorker from './serviceWorker'
 
 const client = new ApolloClient({
   uri: '/api'
 })
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client}>
-    <Listings title="TinyHouse Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
