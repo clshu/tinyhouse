8784793949af93bb674141d328c530fa4296dbda
clshu <clshu2020@gmail.com>
2020-04-03 21:52:19 -0700

    Part 2/4.7: Executing Login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
@@ -11,15 +11,28 @@ import {
   Listings,
   NotFound,
   User,
-  Login
+  Login,
 } from './sections'
 import './styles/index.css'
 
+import { Viewer } from './lib/types'
+
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+}
+
 const client = new ApolloClient({
-  uri: '/api'
+  uri: '/api',
 })
 
 const App = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [viewer, setViewer] = useState<Viewer>(initialViewer)
+
   return (
     <Router>
       <Layout id="app">
@@ -28,7 +41,11 @@ const App = () => {
           <Route exact path="/host" component={Host} />
           <Route exact path="/listing/:id" component={Listing} />
           <Route exact path="/listings/:location?" component={Listings} />
-          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
           <Route exact path="/user/:id" component={User} />
           <Route component={NotFound} />
         </Switch>
