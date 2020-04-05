031e2e00f424306c889725db35f7332560154df3
clshu <clshu2020@gmail.com>
2020-04-04 18:01:50 -0700

    Part 2/4.8 : Building the App Header & Logout

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Layout } from 'antd'
+import { Affix } from 'antd'
 import {
   Home,
   Host,
@@ -12,6 +12,7 @@ import {
   NotFound,
   User,
   Login,
+  AppHeader,
 } from './sections'
 import './styles/index.css'
 
@@ -30,26 +31,26 @@ const client = new ApolloClient({
 })
 
 const App = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [viewer, setViewer] = useState<Viewer>(initialViewer)
 
   return (
     <Router>
-      <Layout id="app">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/host" component={Host} />
-          <Route exact path="/listing/:id" component={Listing} />
-          <Route exact path="/listings/:location?" component={Listings} />
-          <Route
-            exact
-            path="/login"
-            render={(props) => <Login {...props} setViewer={setViewer} />}
-          />
-          <Route exact path="/user/:id" component={User} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
+      <Affix offsetTop={0} className="app__affix-header">
+        <AppHeader viewer={viewer} setViewer={setViewer} />
+      </Affix>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route
+          exact
+          path="/login"
+          render={(props) => <Login {...props} setViewer={setViewer} />}
+        />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   )
 }
