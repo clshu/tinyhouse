70ca86693a65b04c20e745f96e2769ed25e9da37
clshu <clshu2020@gmail.com>
2020-04-05 19:00:22 -0700

    Part 2/6.1: User GraphQL fields.(Server side)

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,4 +1,5 @@
 import merge from 'lodash.merge'
 import { viewerResolver } from './Viewer'
+import { userResolver } from './User'
 
-export const resolvers = merge(viewerResolver)
+export const resolvers = merge(viewerResolver, userResolver)
