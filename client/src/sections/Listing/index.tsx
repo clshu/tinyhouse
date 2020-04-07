4a1f976ec4bcf90824dfe644e5842405160c823e
clshu <clshu2020@gmail.com>
2020-04-07 05:38:11 -0700

    Part 2/7.5: The ListingCreateBooking React component

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
+import { Moment } from 'moment'
 import { RouteComponentProps } from 'react-router-dom'
 import { useQuery } from '@apollo/react-hooks'
 import { Layout, Col, Row } from 'antd'
 import { PageSkeleton, ErrorBanner } from '../../lib/components'
 import { LISTING } from '../../lib/graphql/queries'
-import { ListingDetails, ListingBookings } from './components'
+import {
+  ListingDetails,
+  ListingBookings,
+  ListingCreateBooking,
+} from './components'
 import {
   Listing as ListingData,
   ListingVariables,
@@ -19,6 +24,8 @@ const PAGE_LIMIT = 3
 
 export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
   const [bookingsPage, setBookingsPage] = useState(1)
+  const [checkInDate, setCheckInDate] = useState<Moment | null>(null)
+  const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null)
 
   const { loading, data, error } = useQuery<ListingData, ListingVariables>(
     LISTING,
@@ -49,64 +56,7 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
   }
 
   const listing = data ? data.listing : null
-  //const listingBookings = listing ? listing.bookings : null
-  const listingBookings = {
-    total: 4,
-    result: [
-      {
-        id: '5daa530eefc64b001767247c',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-10-29',
-        checkOut: '2019-10-31',
-        __typename: 'Booking',
-      },
-      {
-        id: '5daa530eefc64b001767247d',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-11-01',
-        checkOut: '2019-11-03',
-        __typename: 'Booking',
-      },
-      {
-        id: '5daa530eefc64b001767247g',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-11-05',
-        checkOut: '2019-11-09',
-        __typename: 'Booking',
-      },
-      {
-        id: '5daa530eefc64b001767247f',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-11-10',
-        checkOut: '2019-11-11',
-        __typename: 'Booking',
-      },
-    ],
-  } as any
+  const listingBookings = listing ? listing.bookings : null
 
   const listingDetailsElement = listing ? (
     <ListingDetails listing={listing} />
@@ -121,12 +71,25 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
     />
   ) : null
 
+  const listingCreateBookingElement = listing ? (
+    <ListingCreateBooking
+      price={listing.price}
+      checkInDate={checkInDate}
+      checkOutDate={checkOutDate}
+      setCheckInDate={setCheckInDate}
+      setCheckOutDate={setCheckOutDate}
+    />
+  ) : null
+
   return (
     <Content className="listings">
       <Row gutter={24} justify="space-between">
         <Col xs={24} lg={14}>
           {listingDetailsElement}
           {listingBookingsElement}
+        </Col>
+        <Col xs={24} lg={10}>
+          {listingCreateBookingElement}
         </Col>
       </Row>
     </Content>
