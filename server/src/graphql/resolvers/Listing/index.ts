09bafc9a94331a665b9c917e512e195873da67c4
clshu <clshu2020@gmail.com>
2020-04-07 21:03:45 -0700

    Part 2/8.2: Building the Listings resolvers

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -2,7 +2,14 @@ import { IResolvers } from 'apollo-server-express'
 import { Request } from 'express'
 import { ObjectId } from 'mongodb'
 import { Listing, Database, User } from '../../../lib/types'
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from './types'
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter,
+} from './types'
 import { authorize } from '../../../lib/utils'
 
 export const listingResolvers: IResolvers = {
@@ -28,8 +35,39 @@ export const listingResolvers: IResolvers = {
         throw new Error(`Failed to query listing: ${error}`)
       }
     },
-    listings: () => {
-      return 'Query.listings'
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: [],
+        }
+
+        let cursor = await db.listings.find({})
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          // filter listings from price low to high
+          cursor = cursor.sort({ price: 1 })
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          // filter listings from price high to low
+          cursor = cursor.sort({ price: -1 })
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+      } catch (error) {
+        throw new Error(`Failed to query user listings: ${error}`)
+      }
     },
   },
   Listing: {
