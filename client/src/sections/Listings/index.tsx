4ebac84ba4b50432ea6d97c29987f8a1d31b24b4
clshu <clshu2020@gmail.com>
2020-04-13 03:44:42 -0700

    Part 2/9.6: Searching for listings from the App Header

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { Link, RouteComponentProps } from 'react-router-dom'
 import { Affix, Layout, List, Typography } from 'antd'
@@ -25,12 +25,18 @@ const { Paragraph, Text, Title } = Typography
 const PAGE_LIMIT = 4
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const locationRef = useRef(match.params.location)
   const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH)
   const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    setPage(1)
+  }, [match.params.location])
 
   const { data, loading, error } = useQuery<ListingsData, ListingsVariables>(
     LISTINGS,
     {
+      skip: locationRef.current !== match.params.location && page !== 1,
       variables: {
         location: match.params.location,
         filter,
