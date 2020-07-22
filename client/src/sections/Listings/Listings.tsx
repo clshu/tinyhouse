39c3e06457581a9a07001bec255204cc81e3a267
clshu <clshu2020@gmail.com>
2020-07-21 20:54:22 -0700

    Par 1/Module 6/Lession 4,5: Query Listings, Abstracting the type of data from server fetch

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,8 +1,35 @@
-import React, { FunctionComponent } from 'react'
+import React from 'react'
+import { server } from '../../lib/api'
+import { ListingsData } from './types'
 
 interface Props {
   title: string
 }
-export const Listings: FunctionComponent<Props> = ({ title }) => {
-  return <h2>{title}</h2>
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS })
+    console.log(data.listings)
+  }
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  )
 }
