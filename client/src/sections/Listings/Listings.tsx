6e1235cb44ee49718ffcd62acec24b347b1023e1
clshu <clshu2020@gmail.com>
2020-07-21 23:22:24 -0700

    Part 1/Module 6/Lesson 6: Delete a listing

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { server } from '../../lib/api'
-import { ListingsData } from './types'
+import {
+  ListingsData,
+  DeleteListingVariables,
+  DeleteListingData,
+} from './types'
 
 interface Props {
   title: string
@@ -21,15 +25,39 @@ const LISTINGS = `
     }
   }
 `
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`
+
 export const Listings = ({ title }: Props) => {
   const fetchListings = async () => {
     const { data } = await server.fetch<ListingsData>({ query: LISTINGS })
     console.log(data.listings)
   }
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: '5f17a9d74ff4e74c9ff8909e',
+      },
+    })
+
+    console.log(data)
+  }
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete Listing!</button>
     </div>
   )
 }
